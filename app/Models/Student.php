8ba37be6ef551a43e1ef7a7020c50fc4451407f4
jabiralawfaa <jadealtero@gmail.com>
2025-09-21 21:00:30 +0700

    CRUD Eloquent ORM

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    // menentukan tabel yang digunakan
+    protected $fillable = ['nama', 'nim', 'jurusan', 'alamat'];
+
+    protected $primaryKey = 'nim';
+    public $incrementing = false;
+    protected $keyType = 'string';
 }
